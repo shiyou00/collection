8c198881cfe0f45f402509beae95d6bdf14e4662
shiyou <8533334@qq.com>
2019-03-21 09:29:11 +0800

    事件对象兼容函数

--- collection.js
+++ collection.js
@@ -46,3 +46,54 @@ function paging(list, page, pageSize) {
     const pageStart = (page - 1) * pageSize;
     return list.slice(pageStart, pageStart + pageSize);
 }
+
+/**
+ * 兼容各个浏览器绑定事件
+ * @type {{addHandler: EventUtil.addHandler, removeHandler: EventUtil.removeHandler}}
+ */
+var EventUtil = {
+
+    addHandler: function(element, type, handler){
+        if (element.addEventListener){
+            element.addEventListener(type, handler, false);
+        } else if (element.attachEvent){
+            element.attachEvent("on" + type, handler);
+        } else {
+            element["on" + type] = handler;
+        }
+    },
+
+    removeHandler: function(element, type, handler){
+        if (element.removeEventListener){
+            element.removeEventListener(type, handler, false);
+        } else if (element.detachEvent){
+            element.detachEvent("on" + type, handler);
+        } else {
+            element["on" + type] = null;
+        }
+    },
+
+    getEvent: function(event){
+        return event ? event : window.event;
+    },
+
+    getTarget: function(event){
+        return event.target || event.srcElement;
+    },
+
+    preventDefault: function(event){
+        if (event.preventDefault){
+            event.preventDefault();
+        } else {
+            event.returnValue = false;
+        }
+    },
+
+    stopPropagation: function(event){
+        if (event.stopPropagation){
+            event.stopPropagation();
+        } else {
+            event.cancelBubble = true;
+        }
+    }
+};
